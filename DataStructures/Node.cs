f20379d0c3b8730dc96d1cbbb4f13477d27b06fb
Rafael Adam Soto <rooksoto@gmail.com>
2017-12-31 00:37:38 -0500

    Node Class
    Node class for node-based lists

---
+++ DataStructures/Node.cs
@@ -0,0 +1,16 @@
+﻿namespace DataStructures
+{
+    internal class Node<T>
+    {
+        internal T Data;
+        internal Node<T> NextNode { get; set; }
+        internal Node<T> PreviousNode { get; set; }
+
+        internal Node(T data, Node<T> nextNode, Node<T> previousNode)
+        {
+            Data = data;
+            NextNode = nextNode;
+            PreviousNode = previousNode;
+        }
+    }
+}
